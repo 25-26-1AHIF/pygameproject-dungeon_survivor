42be47fb96ca0825ccac291fbe3472bf3a2806bb
Alexander Österle <alexander.oesterle@student.htl-rankweil.at>
2026-06-22 08:51:26 +0200

    Unbenutzte Files gelöscht

--- Dungeon_survivor/src/main.py
+++ Dungeon_survivor/src/main.py
@@ -54,11 +54,11 @@ def main_screen(screen, clock):
     coin_int_rect = coins_text.get_rect(center=(GV.SCREEN_WIDTH - 20, 47))
     shop_text_rect = shop_text.get_rect(center=(GV.SCREEN_WIDTH / 2, 100 + 4 * 80))
 
-    image = pygame.image.load("assets/Ninja Adventure - Asset Pack/Ui/Theme/Wip/ThemeDark/nine_path_panel.png").convert_alpha()
+    #image = pygame.image.load("assets/Ninja Adventure - Asset Pack/Ui/Theme/Wip/ThemeDark/nine_path_panel.png").convert_alpha()
     # KI-Anfang
     # KI: ChatGPT
     # prompt: Wie kann ich von diesem rechteck     starten_text_rect = starten_text.get_rect(center=(GV.SCREEN_WIDTH / 2, 100 + 80)) den länge und die breite bekommen
-    res_image = pygame.transform.scale(image, (starten_text_rect.width, starten_text_rect.height))
+    #res_image = pygame.transform.scale(image, (starten_text_rect.width, starten_text_rect.height))
     # KI-Ende
     Sprite_flag = sp(
         filepath="assets/Ninja Adventure - Asset Pack/Backgrounds/Animated/Flag/FlagGray16x16.png",
